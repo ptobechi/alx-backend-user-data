a4275852cb951c6010ee9e90d1f2fb134b15c50b
ghstnet <jamespeteer6363@gmail.com>
2024-07-11 09:38:22 +0100

    basic auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -10,4 +10,28 @@ class BasicAuth(Auth):
     BasicAuth class for basic authentication handling
     Currently empty, to be extended for basic authentication methods.
     """
-    pass
+    def extract_base64_authorization_header(
+                self, authorization_header: str) -> str:
+        """
+        Extracts the Base64 part of the Authorization
+        header for Basic Authentication.
+
+        Args:
+            authorization_header (str): The Authorization
+            header string.
+
+        Returns:
+            str: The Base64 part of the Authorization
+            header, or None if invalid.
+        """
+        if authorization_header is None or not isinstance(
+                    authorization_header, str):
+            return None
+
+        if not authorization_header.startswith("Basic "):
+            return None
+
+        # Get the part after "Basic "
+        base64_part = authorization_header.split(" ")[1].strip()
+
+        return base64_part

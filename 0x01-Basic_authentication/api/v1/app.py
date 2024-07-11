cce8d45b13c2711c94eaeaca7ee1435bcc33587a
ghstnet <jamespeteer6363@gmail.com>
2024-07-11 09:07:54 +0100

    authorization headers

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -20,21 +20,24 @@ if AUTH_TYPE == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
 
+
 @app.before_request
 def before_request():
     if auth is None:
         return
-    
-    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+
+    excluded_paths = ['/api/v1/status/',
+                      '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/']
     if request.path in excluded_paths:
         return
-    
+
     if not auth.require_auth(request.path, excluded_paths):
         abort(401)
-    
+
     if auth.authorization_header(request) is None:
         abort(401)
-    
+
     if auth.current_user(request) is None:
         abort(403)
 

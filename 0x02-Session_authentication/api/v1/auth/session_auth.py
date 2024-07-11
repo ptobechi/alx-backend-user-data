7feefd3640bfaf722459813cce8c8248e280bb10
ghstnet <jamespeteer6363@gmail.com>
2024-07-11 11:06:51 +0100

    task 4

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -36,3 +36,19 @@ class SessionAuth(Auth):
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """
+        Retrieves the user ID associated with a given session ID.
+
+        Args:
+            session_id (str): The session ID to look up.
+
+        Returns:
+            str: The user ID associated with the session ID
+            if found, None otherwise.
+        """
+        if session_id is None or not isinstance(session_id, str):
+            return None
+
+        return self.user_id_by_session_id.get(session_id)

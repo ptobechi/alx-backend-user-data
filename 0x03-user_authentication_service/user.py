8d5467e44655560fabf2e4c0d7782e12ce705cc5
ghstnet <jamespeteer6363@gmail.com>
2024-07-19 05:47:36 +0100

    corrections

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python3
-"""User model for the user authentication service."""
+"""User model for the user
+authentication service."""
 
-from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy import Column, Integer
+from sqlalchemy import String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+
 
 Base = declarative_base()
 
 
 class User(Base):
     """User model for storing user data."""
-    
+
     __tablename__ = 'users'
-    
+  
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
@@ -23,13 +26,13 @@ class User(Base):
 if __name__ == "__main__":
     # SQLite engine for demonstration purposes
     engine = create_engine('sqlite:///:memory:', echo=True)
-    
+
     # Create all tables in the engine
     Base.metadata.create_all(engine)
 
     # Create a configured "Session" class
     Session = sessionmaker(bind=engine)
-    
+
     # Create a Session
     session = Session()
 

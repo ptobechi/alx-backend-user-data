ad748e22b55dea6472a8f996aa426a1cf4cbe66a
ghstnet <jamespeteer6363@gmail.com>
2024-07-19 21:04:18 +0100

    blancl lone fix

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -15,12 +15,13 @@ class User(Base):
     """User model for storing user data."""
 
     __tablename__ = 'users'
-  
+
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
+
 
 # Example usage
 if __name__ == "__main__":

25e8dbfd4f2537e1d186fdd2f3577e70a2f81679
ghstnet <jamespeteer6363@gmail.com>
2024-07-19 21:00:34 +0100

    encrypt password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -22,6 +22,7 @@ def hash_password(password: str) -> bytes:
     hashed = bcrypt.hashpw(password.encode(), salt)
     return hashed
 
+
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """
     Validate that the provided password matches the hashed password.
